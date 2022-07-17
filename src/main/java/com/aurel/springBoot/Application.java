334ee902c84ed8eddc78c39e760bcd40d3fcece7
aurelchivu <aurelchivuutcb@gmail.com>
2022-07-17 15:35:00 +0300

    N Tier Architecture + Implement To Interfaces

--- src/main/java/com/aurel/springBoot/Application.java
+++ src/main/java/com/aurel/springBoot/Application.java
@@ -7,38 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping
-	Customer getCustomer() {
-		return new Customer(1L, "Aurel Chivu");
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Aurel") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	class Customer {
-		private final Long id;
-		private final String name;
-
-		public Customer(Long id, String name) {
-			this.id = id;
-			this.name = name;
-		}
-
-		public Long getId() {
-			return id;
-		}
-
-		public String getName() {
-			return name;
-		}
 	}
 }
---
+++ src/main/java/com/aurel/springBoot/customer/CustomerController.java
@@ -0,0 +1,23 @@
+package com.aurel.springBoot.customer;
+
+import com.aurel.springBoot.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    List<Customer> getCustomer() {
+        return customerService.getCustomer();
+    }
+}
